507e994fb5e9cf4e41c3d9e74ff8c877f4a17e87
Ander <106649207+Andergutobi@users.noreply.github.com>
2023-01-28 04:18:14 +0100

    Update Random_Password.py

--- Random_Password.py
+++ Random_Password.py
@@ -2,14 +2,17 @@ from random import SystemRandom
   
 l = int(input("Cuantos caracteres necesitas?"))
 
-Caracteres = "0123456789:;'#)(][}{><-+-/^%$_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+mi = "abcdefghijklmnopqrstuvwxyz"
+ma = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "0123456789"
+sim = ":;'#)(][}{><-+-/^%$_"
   
 aleatorio = SystemRandom() 
 
 p = " " 
 
 while l > 0: 
- p = p + aleatorio.choice(Caracteres) 
+ p = p + aleatorio.choice(mi+num+ma+sim)
  l=l-1 
   
 print("Tu contraseña segura es:",p)
