977a7e3eb7fc40784851940240502686be950199
Ander <106649207+Andergutobi@users.noreply.github.com>
2023-01-28 02:03:38 +0100

    Un pequeño inicio de codigo para la idea de mi propio gestor de contraseñas.

---
+++ Random_Password.py
@@ -0,0 +1,13 @@
+from random import SystemRandom
+  
+l = int(input("Cuantos caracteres necesitas?"))
+
+Caracteres = "0123456789:;'#)(][}{><-+-/^%$_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  
+aleatorio = SystemRandom() 
+p = " " 
+  
+while l > 0: 
+ p = p + aleatorio.choice(Caracteres) 
+ l=l-1 
+print("Tu contraseña segura es:",p)
